e3663be2dd4d38e7787c43e794fe9d3405d912a7
Gustavo <gustavo.b.moraes2@gmail.com>
2023-03-08 22:10:10 -0300

    Implementação da query com parametros dinamicos e criteria query

--- src/main/java/com/github/bragagustavo/dao/ProductDAO.java
+++ src/main/java/com/github/bragagustavo/dao/ProductDAO.java
@@ -3,7 +3,13 @@ package com.github.bragagustavo.dao;
 import com.github.bragagustavo.shop.model.Product;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProductDAO {
@@ -35,7 +41,7 @@ public class ProductDAO {
                 .getResultList();
     }
 
-    /**Exemplo do uso de uma named query (nao utilizada no restante da aplicação) **/
+    /** Exemplo do uso de uma named query (nao utilizada no restante da aplicação) **/
     public List<Product> findByCategoryName(String name){
         return entityManager.createNamedQuery("Product.findByCategoryName", Product.class)
                 .setParameter("name", name)
@@ -48,5 +54,61 @@ public class ProductDAO {
                 .setParameter("name", name)
                 .getSingleResult();
     }
+
+    public List<Product> findByParameter(String name, BigDecimal price, LocalDate registerDate){
+        String jpql =  " SELECT p FROM Product p WHERE 1=1";
+
+        if (name != null && !name.trim().isEmpty()){
+            jpql = " AND p.name = :name ";
+        }
+
+        if (price != null){
+            jpql = "AND p.price = :price";
+        }
+
+        if (registerDate != null){
+            jpql = "AND p.registerDate = :registerDate";
+        }
+
+        TypedQuery<Product> query = entityManager.createQuery(jpql, Product.class);
+
+        if (name != null && !name.trim().isEmpty()){
+            query.setParameter("name",name);
+        }
+
+        if (price != null){
+            query.setParameter("price",price);
+        }
+
+        if (registerDate != null){
+            query.setParameter("registerDate",registerDate);
+
+        }
+        return query.getResultList();
+    }
+
+    public List<Product> findParameterByCriteria(String name, BigDecimal price, LocalDate registerDate){
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> query = builder.createQuery(Product.class);
+        Root<Product> from = query.from(Product.class);
+
+        Predicate filters = builder.and();
+
+        if (name != null && !name.trim().isEmpty()){
+            filters = builder.and(filters, builder.equal(from.get("name"), name));
+        }
+
+        if (price != null){
+            filters = builder.and(filters, builder.equal(from.get("price"), price));
+        }
+
+        if (registerDate != null){
+            filters = builder.and(filters, builder.equal(from.get("registerDate"), registerDate));
+        }
+        query.where(filters);
+        return  entityManager.createQuery(query).getResultList();
+    }
+
+    //TODO: Testar o Criteria Query
 
 }
